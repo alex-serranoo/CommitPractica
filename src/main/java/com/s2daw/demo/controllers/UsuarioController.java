8e319c8d18569467539ac32fa719c4052785e1df
alex-serranoo <alumno.254662@ies-azarquiel.es>
2023-02-16 10:24:59 +0100

    BorrarUsuarios 2:40

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -2,51 +2,28 @@ package com.s2daw.demo.controllers;
 
 import com.s2daw.demo.dao.UsuarioDao;
 import com.s2daw.demo.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController implements UsuarioDao {
+    @Autowired
+    private UsuarioDao usuarioDao;
     @RequestMapping(value = "usuarios")
     public List <Usuario> getUsuarios(){
-        List <Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(234L);
-        usuario.setNombre("Alex");
-        usuario.setApellidos("Serrano");
-        usuario.setEmail("alejandroserranoinfo@gmail.com");
-        usuario.setPassword("1234");
-        usuario.setTelefono("666666666");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(345L);
-        usuario2.setNombre("Alex");
-        usuario2.setApellidos("Orgaz");
-        usuario2.setEmail("nombre2@gmail.com");
-        usuario2.setPassword("1234");
-        usuario2.setTelefono("666555444");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(356L);
-        usuario3.setNombre("Alex");
-        usuario3.setApellidos("Diaz");
-        usuario3.setEmail("piba@gmail.com");
-        usuario3.setPassword("1234");
-        usuario3.setTelefono("666777888");
-
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
+       return usuarioDao.getUsuario();
     }
-    @RequestMapping(value = "usuario567")
-    public Usuario eliminar(){
+    @RequestMapping (value = "api/usuarios/{id}", method = RequestMethod.GET)
+    private Usuario getUsuario(@PathVariable Long id){
         Usuario usuario = new Usuario();
+        usuario.setId(id);
         usuario.setNombre("Alex");
         usuario.setApellidos("Serrano");
         usuario.setEmail("alejandroserranoinfo@gmail.com");
@@ -54,7 +31,13 @@ public class UsuarioController implements UsuarioDao {
         usuario.setTelefono("666666666");
         return usuario;
     }
-@RequestMapping(value = "usuario312")
+
+
+    @RequestMapping (value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long Id){
+    usuarioDao.eliminar(Id);
+    }
+    @RequestMapping(value = "usuario312")
     public Usuario editar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Alex");
