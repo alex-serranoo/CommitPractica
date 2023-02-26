7f24a4fa4fe6b48bbc5856200ae1d42d4147b216
alex-serranoo <alumno.254662@ies-azarquiel.es>
2023-02-26 18:07:24 +0100

    Añadimos el uso de Argon2 para codificar las contraseñas tambien cambio la base de datos para que quepa todo el codigo hash 3:40

--- src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
+++ src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
@@ -1,5 +1,7 @@
 package com.s2daw.demo.dao;
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,4 +33,20 @@ public class UsuarioDaoImp implements UsuarioDao{
     public void registrar(Usuario usuario){
     entityManager.merge(usuario);
     }
+    public boolean verificarCredenciales(Usuario usuario){
+        String query = "FROM Usuario WHERE email = :email ";
+        List<Usuario> lista= entityManager.createQuery(query)
+            .setParameter("email",usuario.getEmail())
+            .getResultList();
+
+        if (lista.isEmpty()){
+             return  false;
+        }
+
+        String passwordHashed= lista.get(0).getPassword();
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        return argon2.verify(passwordHashed, usuario.getPassword());
+    }
+
 }
